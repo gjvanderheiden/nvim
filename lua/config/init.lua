f423b9b7f2338f8e2ca26732e6fab7822d0fffe4
gjvanderheiden <gjvdspam@gmail.com>
2024-01-10 17:21:20 +0100

    vimsettings in it's own file

--- lua/config/init.lua
+++ lua/config/init.lua
@@ -1,21 +1,6 @@
-vim.g.mapleader = " "
-vim.g.maplocalleader = " "
-
-vim.wo.number = true
-vim.wo.relativenumber = true
-vim.o.clipboard = "unnamedplus"
-vim.o.undofile = true
-vim.o.completeopt = "menuone,noselect"
-vim.o.termguicolors = true
-vim.o.hlsearch = true
-vim.o.ignorecase = true
-vim.o.smartcase = true
-vim.o.tabstop = 2
-vim.o.shiftwidth = 2
-vim.o.softtabstop = 2
-vim.o.expandtab = true
-
+require("config.vimsettings")
 require("config.lazy")
 require("config.autocommands.jdtls")
 require("config.autocommands.highlight-yanked")
 require("config.autocommands.lsp")
+require("config.keymaps")
