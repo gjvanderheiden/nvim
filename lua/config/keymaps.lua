b8e32725b0836afa6382f41609ce14a313e34914
gjvanderheiden <gjvdspam@gmail.com>
2024-01-11 11:24:35 +0100

    settings and keymaps for motions
    trying to disable repeating comments on new lines

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,6 +2,20 @@
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
+-- down and up keeps the cursor in the center
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
+vim.keymap.set("n", "J", "mzJ`z")
+
+-- paste over deletion or something
+vim.keymap.set("x", "<leader>p", [["_dP]])
+-- next greatest remap ever : asbjornHaland
+vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
+vim.keymap.set("n", "<leader>Y", [["+Y]])
+vim.keymap.set({"n", "v"}, "<leader>d", [["_d]])
+
 --Diagnostics
 vim.keymap.set("n", "<space>e", vim.diagnostic.open_float, { desc = "Diagnostic float" })
 vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, { desc = "Goto previous diagnostic" })
--- lua/config/vimsettings.lua
+++ lua/config/vimsettings.lua
@@ -3,14 +3,18 @@ vim.g.maplocalleader = " "
 
 vim.wo.number = true
 vim.wo.relativenumber = true
-vim.o.clipboard = "unnamedplus"
-vim.o.undofile = true
-vim.o.completeopt = "menuone,noselect"
-vim.o.termguicolors = true
-vim.o.hlsearch = true
-vim.o.ignorecase = true
-vim.o.smartcase = true
-vim.o.tabstop = 2
-vim.o.shiftwidth = 2
-vim.o.softtabstop = 2
-vim.o.expandtab = true
+vim.opt.clipboard = "unnamedplus"
+vim.opt.undofile = true
+vim.opt.completeopt = "menuone,noselect"
+vim.opt.termguicolors = true
+vim.opt.hlsearch = true
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.softtabstop = 2
+vim.opt.expandtab = true
+--vim.opt.formatoptions:remove('c')
+--vim.opt.formatoptions:remove('r')
+--vim.opt.formatoptions:remove('o')
+
