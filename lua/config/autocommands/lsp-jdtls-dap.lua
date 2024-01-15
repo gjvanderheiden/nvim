a99c9d5f589729926a78323771b826e79a54f6d5
gertjan <gjvdspam@gmail.com>
2024-01-15 18:25:19 +0100

    group can't be the same or something

--- lua/config/autocommands/lsp-jdtls-dap.lua
+++ lua/config/autocommands/lsp-jdtls-dap.lua
@@ -1,6 +1,5 @@
 vim.api.nvim_create_autocmd("LspAttach", {
-	group = vim.api.nvim_create_augroup("UserLspConfig", {}),
-	callback = function(ev)
+	group = vim.api.nvim_create_augroup("JdtlsMainConfig", {}), callback = function(ev)
 		local clients = vim.lsp.get_active_clients({ name = "jdtls", bufnr = ev.buf })
 		if clients ~= nil and clients[1] ~= nil then
 			require("jdtls.dap").setup_dap_main_class_configs()
