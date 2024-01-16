7305490687dbb2610ae9179145cd488b4cacd105
Gert-Jan van der Heiden <gjvdspam@gmail.com>
2024-01-16 11:42:16 +0100

    added git signs and some keymap changes

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,12 +1,18 @@
---local wk = require("which-key")
+local wk = require("which-key")
+
+local function wk_leader_key(key, key_description)
+  local mapping = {}
+  mapping[key] = {name = key_description}
+  wk.register(mapping, { prefix = "<leader>" })
+end
 
 -- move visual selected text
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
 -- Remap for dealing with word wrap
-vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
-vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
+vim.keymap.set("n", "k", "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
+vim.keymap.set("n", "j", "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
 
 -- down and up keeps the cursor in the center
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
@@ -18,25 +24,28 @@ vim.keymap.set("n", "J", "mzJ`z")
 -- paste over deletion or something
 vim.keymap.set("x", "<leader>p", [["_dP]])
 -- next greatest remap ever : asbjornHaland
-vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
+vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
 vim.keymap.set("n", "<leader>Y", [["+Y]])
 --vim.keymap.set({"n", "v"}, "<leader>d", [["_d]])
 
 --Diagnostics
-vim.keymap.set("n", "<space>e", vim.diagnostic.open_float, { desc = "Diagnostic float" })
+vim.keymap.set("n", "<space>d", vim.diagnostic.open_float, { desc = "Diagnostic float" })
 vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, { desc = "Goto previous diagnostic" })
 vim.keymap.set("n", "]d", vim.diagnostic.goto_next, { desc = "Goto next diagnostic" })
 vim.keymap.set("n", "<space>q", vim.diagnostic.setloclist, { desc = "Diagnostic set loc list" })
 
 --Telescope
+wk_leader_key("s", "[S]earch")
 local builtin = require("telescope.builtin")
-vim.keymap.set("n", "<leader>ff", builtin.find_files, {desc="[f]loat [f]iles"})
-vim.keymap.set("n", "<leader>fg", builtin.live_grep, {desc="[f]loat [g]rep"})
-vim.keymap.set("n", "<leader>fb", builtin.buffers, {desc="[f]loat [b]uffers}"})
-vim.keymap.set("n", "<leader>fh", builtin.help_tags, {desc="[f]loat [h]elp tags"})
+vim.keymap.set("n", "<leader>sf", builtin.find_files, { desc = "[S]earch [f]iles" })
+vim.keymap.set("n", "<leader>sg", builtin.live_grep, { desc = "[S]earch [g]rep" })
+vim.keymap.set("n", "<leader>sb", builtin.buffers, { desc = "[S]earch [b]uffers}" })
+vim.keymap.set("n", "<leader>sh", builtin.help_tags, { desc = "[S]earch [h]elp tags" })
 
---Trouble 
---wk.register("x" ,"Trouble");
+
+--Trouble
+wk_leader_key("x", "Trouble")
+
 local trouble = require("trouble")
 vim.keymap.set("n", "<leader>xx", trouble.toggle, { desc = "Toggle trouble" })
 vim.keymap.set("n", "<leader>xw", function()
