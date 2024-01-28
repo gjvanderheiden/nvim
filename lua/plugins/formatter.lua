ed8ce6f70021a03392cbd1f1486e2e118e75de19
gjvanderheiden <gjvdspam@gmail.com>
2024-01-28 17:27:50 +0100

    added xml formatter

--- lua/plugins/formatter.lua
+++ lua/plugins/formatter.lua
@@ -5,6 +5,10 @@ return {
 		null_ls.setup({
 			sources = {
 				null_ls.builtins.formatting.stylua,
+        null_ls.builtins.formatting.xmlformat.with({
+        extra_args = { "--blanks" },
+    }),
+        null_ls.builtins.diagnostics.tidy,
 			},
 		})
 	end,
