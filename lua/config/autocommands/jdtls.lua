213e6a0ecbb9f1b38649952889531b8861f9aaef
Gert-Jan van der Heiden <gjvdspam@gmail.com>
2024-02-10 11:14:11 +0100

    dap ui starts when starting debug added shortcuts for debug

--- lua/config/autocommands/jdtls.lua
+++ lua/config/autocommands/jdtls.lua
@@ -1,23 +1,22 @@
-
 local function get_bundles()
 	local jdtls_bundles = "/opt/jdtls-bundles"
 	local bundle_dirs = vim.split(vim.fn.glob(jdtls_bundles .. "/*/"), "\n")
 
 	local bundles = {}
 	for _, bundle_dir in pairs(bundle_dirs) do
-    local jars = vim.split(vim.fn.glob(bundle_dir .. "*.jar", true), '\n')
-    vim.list_extend(bundles, jars)
+		local jars = vim.split(vim.fn.glob(bundle_dir .. "*.jar", true), "\n")
+		vim.list_extend(bundles, jars)
 	end
-  return bundles
+	return bundles
 end
 
 local function get_workspace_dir()
-  local project_name = vim.fn.fnamemodify(vim.fn.getcwd(), ":p:h:t")
-  return vim.fn.stdpath("cache") .. "/jdtlsworkspaces/" .. project_name
- end
+	local project_name = vim.fn.fnamemodify(vim.fn.getcwd(), ":p:h:t")
+	return vim.fn.stdpath("cache") .. "/jdtlsworkspaces/" .. project_name
+end
 
 local function add_commands()
-  vim.cmd([[
+	vim.cmd([[
     command! -buffer -nargs=? -complete=custom,v:lua.require'jdtls'._complete_compile JdtCompile lua require('jdtls').compile(<f-args>)
     command! -buffer -nargs=? -complete=custom,v:lua.require'jdtls'._complete_set_runtime JdtSetRuntime lua require('jdtls').set_runtime(<f-args>)
     command! -buffer JdtUpdateConfig lua require('jdtls').update_project_config()
@@ -32,12 +31,11 @@ local jdtls_config = function()
 		cmd = { "jdtls", "-data", get_workspace_dir() },
 		root_dir = vim.fs.dirname(vim.fs.find({ "pom.xml", "gradlew", ".git", "mvnw" }, { upward = true })[1]),
 		init_options = {
-			bundles = get_bundles() ,
+			bundles = get_bundles(),
 		},
 	}
-  add_commands()
-  require("jdtls").start_or_attach(config)
+	add_commands()
+	require("jdtls").start_or_attach(config)
 end
-
 
 vim.api.nvim_create_autocmd("FileType", { pattern = "java", callback = jdtls_config })
--- lua/plugins/nvim-dap.lua
+++ lua/plugins/nvim-dap.lua
@@ -1,5 +1,26 @@
-return { {
-	"mfussenegger/nvim-dap",
-}, {
-	"/rcarriga/nvim-dap-ui",
-} }
+return {
+	{
+		"mfussenegger/nvim-dap",
+	},
+	{
+		"/rcarriga/nvim-dap-ui",
+		dependencies = { "mfussenegger/nvim-dap" },
+		config = function()
+			local dapui = require("dapui")
+			local dap = require("dap")
+			dapui.setup()
+			dap.listeners.before.attach.dapui_config = function()
+				dapui.open()
+			end
+			dap.listeners.before.launch.dapui_config = function()
+				dapui.open()
+			end
+			dap.listeners.before.event_terminated.dapui_config = function()
+				dapui.close()
+			end
+			dap.listeners.before.event_exited.dapui_config = function()
+				dapui.close()
+			end
+		end,
+	},
+}
